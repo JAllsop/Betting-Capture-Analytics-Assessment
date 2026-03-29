643eb02f374eed6546f4de5632ab961e41a7ef98
JAllsop <james.allsop8@gmail.com>
2026-03-29 18:11:39 +0200

    feat: add docker-compose and .net aspire orchestration

--- src/OT.Assessment.Consumer/Program.cs
+++ src/OT.Assessment.Consumer/Program.cs
@@ -1,18 +1,14 @@
 ﻿using Microsoft.Extensions.Configuration;
 
-var host = Host.CreateDefaultBuilder(args)
-    .ConfigureAppConfiguration(config =>
-    {
-        config.SetBasePath(Directory.GetCurrentDirectory())
-            .AddJsonFile("appsettings.json")
-            .Build();
-    })
-    .ConfigureServices((context, services) =>
-    {
-        //configure services
-      
-    })
-    .Build();
+var builder = Host.CreateApplicationBuilder(args);
+
+builder.Configuration
+    .SetBasePath(Directory.GetCurrentDirectory())
+    .AddJsonFile("appsettings.json");
+
+builder.AddServiceDefaults();
+
+var host = builder.Build();
 
 var logger = host.Services.GetRequiredService<ILogger<Program>>();
 logger.LogInformation("Application started {time:yyyy-MM-dd HH:mm:ss}", DateTime.Now);
